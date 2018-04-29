4037894aec8b8fc5ec645af684346a3a7cb34345
Pratham <prathamv28@gmail.com>
2018-04-29 17:06:13 +0530

    Update Main to return IO()

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,13 @@ import Board
 import Moves
 import Gameplay
 
-main :: IO Move
+main :: IO ()
 main = do showBoard initBoard
           putStrLn ((replicate 21 '-') ++ "  Welcome to Reversi !!  " ++ (replicate 21 '-'))
           gp   <- promptOptions "Gameplay Mode" gameplayOpts ""
           coin <- promptOptions "Player 1 Coin" coinOpts ""
-          case gp of
-            VsPlayer -> playTwoPlayer coin
-            VsCPU    -> do level <- promptOptions "Level" levelOpts ""
-                           playWithAI level coin
+          p <- case gp of
+                 VsPlayer -> playTwoPlayer coin
+                 VsCPU    -> do level <- promptOptions "Level" levelOpts ""
+                                playWithAI level coin
+          return ()
